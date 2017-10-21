197bf3cff3ac02bbd5d3125698c4ecf547f42d7a
Steven McCreery <logad8@gmail.com>
2017-10-21 03:45:17 -0400

    changed database function outside of click function and then restructured js file. added background image and some extra styling.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,13 +24,13 @@ $(document).ready(function() {
 			$("#schedule").addClass("col-xs-8");
 			$("#schedule").removeClass("col-xs-12");
 		}
+		$("#schedule").removeClass("hidden");
 	})
 
 	// change display mode
 	$("#viewChange").on("click", function() {
 		selectViewModal();
 	})
-
 
 	// Initialize Firebase
 	var config = {
@@ -46,23 +46,15 @@ $(document).ready(function() {
 	// define database
 	var database = firebase.database();
 
-	
-
 	// push to firebase and prevent submit's default reload
 	$("#add-train").on("click", function(event) {
-		$("tbody").empty();
 		event.preventDefault();
 		var trainName = $("#name-input").val().trim();
 		var destination = $("#destination-input").val().trim();
 		var firstTrainTime = $("#ftt-input").val().trim();
 		var frequency = $("#frequency-input").val().trim();
 
-		// momentjs items
-		
-		// var yesterDay;
-		// var timeDiff;
-		// var remainder;
-
+		// add new inputs to firebase
 		database.ref().push({
 			trainName: trainName,
 			destination: destination,
@@ -71,33 +63,30 @@ $(document).ready(function() {
 			dateAdded: firebase.database.ServerValue.TIMESTAMP
 		});
 
-		// clear input sections back to placeholder
+		// clear input sections and table, then redraw table
 		$("input").val("");
-
-
-		// ordering display area
-		database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function(snapshot) {
-			var timeNow = moment().format("HHmm");
-			var yesterDay = moment(snapshot.val().firstTrainTime, "HHmm").subtract(1,"Day");
-			var timeDiff = moment(yesterDay, "minutes").diff(timeNow, "minutes");
-			console.log(timeDiff);
-			var remainder = timeDiff % snapshot.val().frequency;
-			console.log(remainder);
-			var nextTrainWait = snapshot.val().frequency - remainder;
-			var nextTrainArrive = moment().add(nextTrainWait, "minutes").format("HHmm");
-			$("tbody").prepend(
-				"<tr><td>" + snapshot.val().trainName + "</td><td>" + 
-				snapshot.val().destination + "</td><td>" + 
-				snapshot.val().frequency + "</td><td>" + 
-				nextTrainArrive + "</td><td>" + 
-				nextTrainWait + "</td></tr>"
-			)
-		})
+		$("tbody").empty();
+		runtime();
 	})
 
-
-
-
+	// ordering display area
+	var runtime = function() {
+		database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function(snapshot) {
+		var timeNow = moment().format("HHmm");
+		var yesterDay = moment(snapshot.val().firstTrainTime, "HHmm").subtract(1,"Day");
+		var timeDiff = moment(yesterDay, "minutes").diff(timeNow, "minutes");
+		var remainder = timeDiff % snapshot.val().frequency;
+		var nextTrainWait = snapshot.val().frequency - remainder;
+		var nextTrainArrive = moment().add(nextTrainWait, "minutes").format("HHmm");
+		$("tbody").prepend(
+			"<tr><td>" + snapshot.val().trainName + "</td><td>" + 
+			snapshot.val().destination + "</td><td>" + 
+			snapshot.val().frequency + "</td><td>" + 
+			nextTrainArrive + "</td><td>" + 
+			nextTrainWait + "</td></tr>"
+		)
+	})} 
+	runtime();
 
 })
 
